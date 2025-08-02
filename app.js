1a2cf36bef44d33186b447d9818b34e170cd320e
Emmanuel Joshimar Ortega Garcia <joshimar.orteg.garcia@gmail.com>
2025-08-01 23:40:13 -0500

    git

--- app.js
+++ app.js
@@ -1,1 +1,29 @@
 // El principal objetivo de este desafío es fortalecer tus habilidades en lógica de programación. Aquí deberás desarrollar la lógica para resolver el problema.
+// Esto crea la lista
+let nombresAgregados = [];
+
+
+function añadirAmigo() {
+    let añadir = document.getElementById('amigo').value;
+    if (nombresAgregados.includes(añadir))
+        return alert ("Este nombre ya fue ingresado")
+    
+        if (añadir !== "") {
+            nombresAgregados.push(añadir);
+
+            console.log(nombresAgregados)
+            console.log(nombresAgregados.length)/* Solo para revisar que este bien hecho*/ 
+            limpiarCaja();
+        } else {
+            alert ("Por favor, escribe un nombre");
+            
+            
+            return;
+        }
+    }
+
+
+function limpiarCaja() {
+    document.querySelector('#amigo').value = "";
+    return;
+}
